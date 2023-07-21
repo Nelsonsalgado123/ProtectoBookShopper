684859db26c98c47ac8cd5ace827de5a2c6ffed2
jherson17 <92492116+jherson17@users.noreply.github.com>
2023-07-21 13:05:04 -0500

    Delete public/assets directory

--- public/assets/application-fe7445c3cde74ee216e74b816a42d85307f36687c56214b85396f8dda8b4a6ae.js
+++
@@ -1,7 +0,0 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-import "./jquery.min.js"
-import "./bootstrap.bundle.min.js"
-import "./sb-admi-2.min.js"
-;
