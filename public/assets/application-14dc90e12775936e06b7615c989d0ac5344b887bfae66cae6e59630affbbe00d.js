2f8ba265e396976ceff78f3321d6585d6267762b
jherson17 <deluquezjherson@gmail.com>
2023-07-21 12:56:29 -0500

    2

---
+++ public/assets/application-14dc90e12775936e06b7615c989d0ac5344b887bfae66cae6e59630affbbe00d.js
@@ -0,0 +1,6 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+import "./jquery.min.js"
+import "./bootstrap.bundle.min.js"
+import "./sb-admi-2.min.js";
